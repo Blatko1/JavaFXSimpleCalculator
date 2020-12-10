fe516635115f4eb0d580062f98a58767c58d2a18
Blatko1 <kings.of.funny.gemplays@gmail.com>
2020-12-10 13:04:52 +0100

    Added a function to plus button.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,45 +1,52 @@
 package sample;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 
 public class Controller {
 
     public Button basta;
-    private float currentNum;
+    public TextField textField;
+    private float currentNum = 0;
+    private float result;
     private StringBuilder operations = new StringBuilder();
 
     public void CancleButton() throws Exception {
-
+        currentNum = 0;
+        operations.delete(0, operations.length());
     }
     public void OneButton() throws Exception{
         operations.append("1");
     }
     public void TwoButton() throws Exception{
-
+        operations.append("2");
     }
     public void ThreeButton() throws Exception{
-
+        operations.append("3");
     }
     public void FourButton() throws Exception{
-
+        operations.append("4");
     }
     public void FiveButton() throws Exception{
-
+        operations.append("5");
     }
     public void SixButton() throws Exception{
-
+        operations.append("6");
     }
     public void SevenButton() throws Exception{
-
+        operations.append("7");
     }
     public void EightButton() throws Exception{
-
+        operations.append("8");
     }
     public void NineButton() throws Exception{
-
+        operations.append("9");
     }
     public void ZeroButton() throws Exception{
-
+        operations.append("0");
     }
     public void MultyButton() throws Exception{
 
@@ -51,14 +58,19 @@ public class Controller {
 
     }
     public void PlusButton() throws Exception{
-        currentNum = operations
+        operations.append("+");
+     /*   result = currentNum + Float.parseFloat(operations.toString());
+        currentNum = result;
+        operations.delete(0, operations.length());*/
     }
     public void DecimalButton() throws Exception{
-
+        operations.append(".");
     }
     public void EqualsButton() throws Exception{
-        float result = Integer.parseInt(operations.toString());
-        System.out.println(result);
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        System.out.println(engine.eval(operations.toString()));
+      //  textField.setText(Float.toString(result));
         operations.delete(0, operations.length());
     }
 }
